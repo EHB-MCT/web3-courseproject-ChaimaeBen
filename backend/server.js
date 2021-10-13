19d33c0c9ddeb7a9d014789bfa4b06ab4f17aa46
machai <chaimae.benhammou@student.ehb.be>
2021-10-13 09:44:41 +0200

    backend changed

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,9 @@ app.post('/api/upload', async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log('listening on 3001');
+    console.log('listening on '+ port);
 });
 // create a GET route
 app.get('/', (req, res) => { //Line 9
