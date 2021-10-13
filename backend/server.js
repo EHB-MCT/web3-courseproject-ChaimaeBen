38dabdbcd490d5dc6cf2d535cf6525d85f4b0505
machai <chaimae.benhammou@student.ehb.be>
2021-10-13 14:13:16 +0200

    info backend

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,43 @@
-const { cloudinary } = require('./utils/cloudinary');
-const express = require('express');
+const { cloudinary } = require("./utils/cloudinary");
+const express = require("express");
 const app = express();
-var cors = require('cors');
+var cors = require("cors");
 
-app.use(express.static('public'));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.static("public"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 
-
-app.post('/api/upload', async (req, res) => {
-    try {
-        const file = req.body.data;
-       const response = (file,{
-           upload_preset:'3d-models'
-       })
-       console.log(response);
-        res.json({ msg: response });
-        res.status(500).json({err:"somehting when wrong"})
-    } catch (err) {
-        console.error(err);
-    }
+app.post("/api/upload", async (req, res) => {
+  try {
+    const file = req.body.data;
+    const response =
+      (file,
+      {
+        upload_preset: "3d-models",
+      });
+    console.log(response);
+    res.json({ msg: response });
+    res.status(500).json({ err: "somehting when wrong" });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
+app.get("/api/upload", async (req, res) => {
+  try {
+      console.log('the upload file')
+    res.json({ msg: "you got the upload file" });
+  } catch (err) {
+    console.error(err);
+  }
+});
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log('listening on '+ port);
+  console.log("listening on " + port);
 });
 // create a GET route
-app.get('/', (req, res) => { //Line 9
-  res.send({ express: 'YOUR BACKEND changed again  IS CONNECTED TO REACT' }); //Line 10
+app.get("/", (req, res) => {
+  //Line 9
+  res.send({ info: "YOUR BACKEND changed again  IS CONNECTED TO REACT" }); //Line 10
 }); //L
