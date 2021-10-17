9438ca941bd66e6a7feef922a321e7fb7db0f9ff
machai <chaimae.benhammou@student.ehb.be>
2021-10-17 13:15:02 +0200

    change home frontend

--- frontend/src/pages/homePage.js
+++ frontend/src/pages/homePage.js
@@ -2,22 +2,23 @@ import React, { useState, useEffect } from "react";
 //import { Image } from 'cloudinary-react';
 
 export default function Home() {
-  const [msg, setmsg] = useState("");
+  const [msg, setmsg] = useState([]);
 
   useEffect(() => {
+      console.log('here is info')
     // GET request using fetch inside useEffect React hook
     fetch("https://upload-3d-backend.herokuapp.com/api/upload")
       .then((response) => response.json())
       .then((data) => {
-        console.log("here is the data ");
-        setmsg(data);
+        console.log("here is the data " + JSON.stringify(data.msg));
+        setmsg(data.msg);
       });
   }, []);
 
   return (
     <div>
       <h1>new home hoem here </h1>
-      {msg && <p class="msg">{msg}</p>}
+      <p>The message = {msg}</p>
     </div>
   );
 }
